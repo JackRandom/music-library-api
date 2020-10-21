494d2e53747aecbca2b01ba76ed515b4595f423f
JackRandom <andrewglogan1987@gmail.com>
2020-10-21 20:03:27 +0100

    using heruko on my music library API

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,14 @@ const Sequelize = require('sequelize');
 const ArtistModel = require('./artist');
 const AlbumModel = require('./album');// new
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, CLEARDB_DATABASE_URL } = process.env;
+// const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const setupDatabase = () => {
-  const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  const connection = CLEARDB_DATABASE_URL ?
+  new Sequelize(CLEARDB_DATABASE_URL) :
+  new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     port: DB_PORT,
     dialect: 'mysql',
@@ -15,12 +19,13 @@ const setupDatabase = () => {
   const Artist = ArtistModel(connection, Sequelize);
   const Album = AlbumModel(connection, Sequelize);
 
-  Album.belongsTo(Artist, { as: 'artist' }); // new line unsure
+  Album.belongsTo(Artist, { as: 'artist' }); 
+  // Song.belongsTo(Album, { as: 'album' }); 
 
   connection.sync({ alter: true });
   return {
     Artist,
-    Album // issue here ask on Saturday?
+    Album 
   };
 };
 
